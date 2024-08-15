511c71fe5d4064e40b78f1d66f59a65d1419406d
AndyFJ <andresfernandez260@gmail.com>
2024-08-15 11:58:14 -0400

    Base de datos

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,7 +5,7 @@ class Conexion{
 	static public function conectar(){
 		$link = new PDO("mysql:host=localhost;dbname=u767726139_gse",
 			"u767726139_admin",
-            "^4r+qzM$Z");
+            "&Kg66bp30;");
 		$link->exec("set names utf8");
 		return $link;
 	}
