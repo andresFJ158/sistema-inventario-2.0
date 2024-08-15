7bb0f98007983107c8b991275ffd6cb0dfdff1f1
AndyFJ <andresfernandez260@gmail.com>
2024-08-15 11:48:16 -0400

    Quitar Facturacion ejemplos Conexion DB

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,9 +3,9 @@
 class Conexion{
 
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=u767726139_sistema",
-			"u767726139_andres",
-            "kMoV:TR>D8");
+		$link = new PDO("mysql:host=localhost;dbname=u767726139_gse",
+			"u767726139_admin",
+            "^4r+qzM$Z");
 		$link->exec("set names utf8");
 		return $link;
 	}
